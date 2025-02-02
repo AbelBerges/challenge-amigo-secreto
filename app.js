5e95add44be47d41e351dd919e6c9f652d39b9d0
abel <abel.berges@gmail.com>
2025-02-02 06:55:12 -0300

    Mejoras en el funcionamiento del juego

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
-
-;
+let ingresoNom = document.getElementById('amigo');
 let resultadoNom = document.getElementById('resultado');
-let cantidadAmigos = 0;
+let cantidad;
+let vaciar = ""
 let elegido = 0;
 function agregarAmigo(){
-    let entradaNombre = document.getElementById('amigo').value
+    let entradaNombre = '';
+    entradaNombre = document.getElementById('amigo').value;
+    let mayus = entradaNombre.toUpperCase();
     let elemento = document.createElement('div');
+    let testigo = '';
     //console.log('El valor de nombre ' + entradaNombre);
     if (entradaNombre == ''){
         alert('El nombre no puede estar vacío');
     } else {
-        listaAmigos.push(entradaNombre);
-        cantidadAmigos++;
-        elemento.innerHTML = `<span>${entradaNombre}`;
-        resultadoNom.appendChild(elemento);
+        if (listaAmigos.length == 0 ) {
+            resultadoNom.innerHTML = '';
+            listaAmigos.push(mayus);
+            elemento.innerHTML = `<span>${mayus}`;
+            resultadoNom.appendChild(elemento);
+        } else {
+            for (let index = 0; index < listaAmigos.length; index++) {
+                if(mayus == listaAmigos[index]){
+                    testigo = listaAmigos[index];
+                }
+            }
+            if(testigo == ''){
+                listaAmigos.push(mayus);
+                elemento.innerHTML = `<span>${mayus}`;
+                resultadoNom.appendChild(elemento);
+            } else {
+                alert(`El amigo/a ${mayus} ya fue añadido/a`);
+            }
+        }
     }
 }
 
 function sortearAmigo(){
-    if(cantidadAmigos <= 1){
+    cantidad = listaAmigos.length;
+    if(listaAmigos.length < 1){
         alert('Debe ingresar por lo menos dos amigos');
     } else {
-        elegido = parseInt((Math.random() * cantidadAmigos) +1);
-        for(let i = 0; i < cantidadAmigos;i++){
+        elegido = parseInt((Math.random() * cantidad));
+        console.log(elegido);
+        for(let i = 0; i < listaAmigos.length;i++){
+            console.log(listaAmigos[i]);
             if (i == elegido){
-                //resultadoNom.remove();
                 resultadoNom.innerHTML = `El amigo elegido es ${listaAmigos[i]}`;
             }
         }
     }
+    listaAmigos = [];
+    ingresoNom.value = vaciar;
+}
+
+function reiniciar(){
+    listaAmigos = [];
+    resultadoNom.innerHTML = "";
+    ingresoNom.value = vaciar;
 }
